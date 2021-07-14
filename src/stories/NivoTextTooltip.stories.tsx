4d84b56c2c835b47e51cf957f0446ba1bb2dec2f
John <johnvfs@protonmail.com>
2021-07-14 16:41:15 -0300

    Setup StoryBook for component documentation and testing

---
+++ src/stories/NivoTextTooltip.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import NivoTextTooltip from '../components/graphs/NivoTextTooltip'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+export default {
+  title: 'Graphs/NivoTextTooltip',
+  component: NivoTextTooltip,
+  argTypes: {
+    color: { control: 'color' }
+  }
+} as ComponentMeta<typeof NivoTextTooltip>
+
+const Template: ComponentStory<typeof NivoTextTooltip> = (args) => (
+  <NivoTextTooltip {...args} />
+)
+
+export const StringValue = Template.bind({})
+StringValue.args = {
+  text: 'Some value',
+  value: '55.45',
+  color: '#fff'
+}
+
+export const PercentageValue = Template.bind({})
+PercentageValue.args = {
+  text: 'Some percentage value',
+  value: 0.55,
+  color: '#fff',
+  isPercentage: true
+}
