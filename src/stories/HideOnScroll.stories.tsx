22d26bf6df7bb96844be114bb0d15f427d52ba6c
John <johnvfs@protonmail.com>
2021-08-05 13:42:46 -0300

    Add documentation Story to HideOnScroll component

---
+++ src/stories/HideOnScroll.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { ComponentStory, ComponentMeta, Story } from '@storybook/react'
+import { AppBar, Card, CardContent } from '@material-ui/core'
+
+import HideOnScroll, { Props } from '../components/HideOnScroll'
+
+export default {
+  title: 'Other/HideOnScroll',
+  component: HideOnScroll
+} as ComponentMeta<typeof HideOnScroll>
+
+const Template: ComponentStory<typeof HideOnScroll> = (args) => {
+  return (
+    <Card variant='outlined'>
+      <CardContent style={{ height: 600 }}>
+        Scroll down
+        <HideOnScroll {...args} />
+      </CardContent>
+    </Card>
+  )
+}
+
+export const Example: Story<Props> = Template.bind({})
+Example.args = {
+  children: (
+    <AppBar>
+      I disappear when scrolling down, and appear back scrolling up
+    </AppBar>
+  )
+}
