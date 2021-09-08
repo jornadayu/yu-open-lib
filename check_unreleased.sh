b2bf0580cde8c6cf80ffb2f28623abca0b13e7e3
John <johnvfs@protonmail.com>
2021-09-08 17:13:28 -0300

    Add preversion script to check for unreleased stories

---
+++ check_unreleased.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ORANGE=$'\e[33m'
+GREEN=$'\e[32m'
+RED=$'\e[31m'
+NC=$'\e[0m'
+
+echo -n $ORANGE
+
+if grep -lnr --include \*.tsx\* 'badges: \[BADGES.UNRELEASED\]' 'src/stories';
+then
+  echo $NC
+  echo "${RED}Error: The Stories above are marked as unreleased, you need to version them first since you are creating a new release${NC}"
+  echo "Read more: https://yu-open-lib.netlify.app (at the 'Guidelines' tab)"
+  exit 1
+else
+  echo $NC
+  echo "${GREEN}No Stories marked as released${GREEN}"
+  exit 0
+fi
