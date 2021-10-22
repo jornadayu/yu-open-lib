ee31c9596042bcf132135a61239c995e9c80a237
John Victor <johnvictorfs@gmail.com>
2021-10-22 14:26:53 -0300

    Add WhatsappChip story

---
+++ src/stories/WhatsappChip.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { ComponentStory, ComponentMeta, Story } from '@storybook/react'
+import { Card, CardContent } from '@mui/material'
+
+import WhatsappChip, { Props } from '../components/WhatsappChip'
+
+export default {
+  title: 'Components/WhatsappChip',
+  component: WhatsappChip
+} as ComponentMeta<typeof WhatsappChip>
+
+const Template: ComponentStory<typeof WhatsappChip> = (args) => {
+  return (
+    <Card variant='outlined'>
+      <CardContent>
+        <WhatsappChip {...args} />
+      </CardContent>
+    </Card>
+  )
+}
+
+export const Example: Story<Props> = Template.bind({})
+Example.args = {
+  number: '551111111111111',
+  message: 'Some Message'
+}
+
+export const OnClickEvent: Story<Props> = Template.bind({})
+OnClickEvent.args = {
+  number: '551111111111111',
+  message: 'Some Message',
+  onClick: () => alert('onClick')
+}
