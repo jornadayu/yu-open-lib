20f4f9707450860cdebcb414340001aea3adb2e4
John Victor <johnvictorfs@gmail.com>
2021-10-22 14:57:58 -0300

    Update YuGrid styling

--- src/components/YuGrid.tsx
+++ src/components/YuGrid.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
+import { styled } from '@mui/material/styles'
+
 import Grid, { GridTypeMap } from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
-import makeStyles from '@mui/styles/makeStyles'
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
+const PREFIX = 'YuGrid'
+
+const classes = {
+  paper: `${PREFIX}-paper`
+}
+
+const StyledGrid = styled(Grid)(({ theme }) => ({
+  [`& .${classes.paper}`]: {
     padding: theme.spacing(2),
     background: theme.palette.background.default,
-    height: '100%',
-    textAlign: ({ centerText }: { centerText: Props['centerText'] }) =>
-      centerText ? 'center' : 'initial'
+    height: '100%'
   }
 }))
 
@@ -27,16 +32,22 @@ const YuGrid: React.FC<Props> = ({
   itemProps,
   centerText = false
 }) => {
-  const classes = useStyles({ centerText })
-
   return (
-    <Grid container spacing={3} justifyContent='center' {...containerProps}>
+    <StyledGrid
+      container
+      spacing={3}
+      justifyContent='center'
+      sx={{
+        textAlign: centerText ? 'center' : 'initial'
+      }}
+      {...containerProps}
+    >
       <Grid item xs={12} {...itemProps}>
         <Paper elevation={3} className={classes.paper}>
           {children}
         </Paper>
       </Grid>
-    </Grid>
+    </StyledGrid>
   )
 }
 
