4e58b75f0183365b4813b0814fcaceee331f71db
John Victor <johnvictorfs@gmail.com>
2021-10-22 13:38:17 -0300

    Run mui v5 codemod in example folder

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { Typography, CssBaseline } from '@material-ui/core'
-import { ThemeProvider } from '@material-ui/core/styles'
-import GroupIcon from '@material-ui/icons/Group'
+import { Typography, CssBaseline } from '@mui/material'
+import { ThemeProvider, Theme, StyledEngineProvider } from '@mui/material/styles';
+import GroupIcon from '@mui/icons-material/Group'
 
 // @ts-ignore
 import { YuToastProvider, CopyableInput, AppTheme, YuAppBar, WhatsappChip, YuNegativeLaranja } from 'yu-open-lib'
 import 'yu-open-lib/dist/index.css'
 
 import ToastTests from './ToastTests'
+
+
+declare module '@mui/styles/defaultTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends Theme {}
+}
+
 
 const theme = AppTheme({ darkMode: true })
 
@@ -39,25 +46,27 @@ const App = () => {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <YuToastProvider>
-        <YuAppBar backgroundColor="#070707" items={items} loggedIn logo="yu" searchBar={false} />
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <YuToastProvider>
+          <YuAppBar backgroundColor="#070707" items={items} loggedIn logo="yu" searchBar={false} />
 
-        <div style={{ marginTop: 100 }}>
-          <img src={YuNegativeLaranja} alt="yu-negative-orange-logo" width="64" height="64" />
+          <div style={{ marginTop: 100 }}>
+            <img src={YuNegativeLaranja} alt="yu-negative-orange-logo" width="64" height="64" />
 
-          <Typography variant="h2">Texto exemplo</Typography>
-        </div>
+            <Typography variant="h2">Texto exemplo</Typography>
+          </div>
 
-        <CopyableInput text="url.com" label="Some URL" />
+          <CopyableInput text="url.com" label="Some URL" />
 
-        <ToastTests />
+          <ToastTests />
 
-        <WhatsappChip onClick={() => {}} number="123456" message="Olá" />
-      </YuToastProvider>
-    </ThemeProvider>
-  )
+          <WhatsappChip onClick={() => {}} number="123456" message="Olá" />
+        </YuToastProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export default App
--- example/src/ToastTests.tsx
+++ example/src/ToastTests.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Button } from '@material-ui/core'
+import { Button } from '@mui/material'
 import { useToasts } from 'react-toast-notifications'
 
 const ToastTests: React.FC = () => {
