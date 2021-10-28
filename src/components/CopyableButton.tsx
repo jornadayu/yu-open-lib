67fb12bfbe40692d9490452a92443f8775a3045e
John Victor <johnvictorfs@gmail.com>
2021-10-28 10:40:25 -0300

    Add check icon to CopyableButton

--- src/components/CopyableButton.tsx
+++ src/components/CopyableButton.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
-import { FileCopy as FileCopyIcon } from '@mui/icons-material'
+import {
+  FileCopy as FileCopyIcon,
+  CheckCircleOutline as CheckCircleOutlineIcon
+} from '@mui/icons-material'
 
 import { copyToClipboard } from '../helpers/text'
 
@@ -12,12 +15,22 @@ export type Props = {
   tooltip?: string
 }
 
-const CopyableButton: React.FC<Props> = ({ text, tooltip = 'Copiar' }) => (
-  <Tooltip title={tooltip}>
-    <IconButton onClick={() => copyToClipboard(text as string)}>
-      <FileCopyIcon />
-    </IconButton>
-  </Tooltip>
-)
+const CopyableButton: React.FC<Props> = ({ text, tooltip = 'Copiar' }) => {
+  const [copied, setCopied] = useState(false)
+
+  const copyText = () => {
+    copyToClipboard(text)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1000)
+  }
+
+  return (
+    <Tooltip title={tooltip}>
+      <IconButton onClick={copyText}>
+        {copied ? <CheckCircleOutlineIcon /> : <FileCopyIcon />}
+      </IconButton>
+    </Tooltip>
+  )
+}
 
 export default CopyableButton
