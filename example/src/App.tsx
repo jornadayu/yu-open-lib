b86f6dee673552b0fd64bad35d05ce4bbfde9ba4
John Victor <johnvictorfs@gmail.com>
2021-10-28 11:35:52 -0300

    Add default import for missing font

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import { Typography, CssBaseline, Switch, FormGroup, FormControlLabel } from '@mui/material'
+import { Box } from '@mui/system'
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
 import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming'
 import GroupIcon from '@mui/icons-material/Group'
@@ -11,8 +12,6 @@ import { YuToastProvider, CopyableInput, AppTheme, YuAppBar, WhatsappChip, useTo
 import 'yu-open-lib/dist/index.css'
 
 import ToastTests from './ToastTests'
-
-
 
 const App = () => {
   const [darkMode, toggleDarkMode] = useToggle(true)
@@ -77,6 +76,56 @@ const App = () => {
             <FormGroup>
               <FormControlLabel control={<Switch checked={darkMode} onChange={toggleDarkMode} />} label="Dark Mode" />
             </FormGroup>
+
+            <Box sx={{ width: '100%', maxWidth: 500 }}>
+              <Typography variant="h1" component="div" gutterBottom>
+                h1. Heading
+              </Typography>
+              <Typography variant="h2" gutterBottom component="div">
+                h2. Heading
+              </Typography>
+              <Typography variant="h3" gutterBottom component="div">
+                h3. Heading
+              </Typography>
+              <Typography variant="h4" gutterBottom component="div">
+                h4. Heading
+              </Typography>
+              <Typography variant="h5" gutterBottom component="div">
+                h5. Heading
+              </Typography>
+              <Typography variant="h6" gutterBottom component="div">
+                h6. Heading
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur
+              </Typography>
+              <Typography variant="subtitle2" gutterBottom component="div">
+                subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+                neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+                quasi quidem quibusdam.
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+                neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+                quasi quidem quibusdam.
+              </Typography>
+              <Typography variant="button" display="block" gutterBottom>
+                button text
+              </Typography>
+              <Typography variant="caption" display="block" gutterBottom>
+                caption text
+              </Typography>
+              <Typography variant="overline" display="block" gutterBottom>
+                overline text
+              </Typography>
+            </Box>
           </YuToastProvider>
         </ThemeProvider>
       </EmotionThemeProvider>
