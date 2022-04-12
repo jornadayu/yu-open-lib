1f3b454d20ef7d2daa3fac666df4b0ba2b6ef2ba
Nicolas <nicolasalexandre0001@gmail.com>
2022-04-12 19:12:38 -0300

    rich text viewer

---
+++ src/stories/utils/RichTextViewer.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import { ComponentMeta, ComponentStory, Story } from '@storybook/react'
+
+import { BADGES } from '../../../.storybook/constants'
+import RichTextViewer, { Props } from '../../components/RichTextViewer'
+
+export default {
+  title: 'Utils/RichTextViewer',
+  component: RichTextViewer,
+  parameters: {
+    badges: [BADGES.UNRELEASED]
+  }
+} as ComponentMeta<typeof RichTextViewer>
+
+const Template: ComponentStory<typeof RichTextViewer> = (args) => {
+  return (
+    <div>
+      Rich text viewer
+      <RichTextViewer {...args} />
+    </div>
+  )
+}
+
+export const Example: Story<Props> = Template.bind({})
+Example.args = {
+  html: '<h1>Title</h1><p>Some text</p><ol><li>item</li><li>item</li>'
+}
+
+export const WithoutYuMode: Story<Props> = Template.bind({})
+WithoutYuMode.args = {
+  html: '<h1>Title</h1><p>Some text</p><ol><li>item</li><li>item</li>',
+  yuMode: false
+}
+
+export const WithMaxHeight: Story<Props> = Template.bind({})
+WithMaxHeight.args = {
+  html: '<h1>Title</h1><p>Some text</p><ol><li>item</li><li>item</li>',
+  maxHeight: 50
+}
+
+export const WithCustomStyle: Story<Props> = Template.bind({})
+WithCustomStyle.args = {
+  html: '<h1>Title</h1><p>Some text</p><ol><li>item</li><li>item</li>',
+  customStyle: {
+    backgroundColor: 'pink'
+  }
+}
