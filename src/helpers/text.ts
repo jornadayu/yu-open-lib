f6b5f0ffb6da66ab0cf22e2ea8b643c17486bf4c
Nicolas <nicolasalexandre0001@gmail.com>
2022-04-12 18:12:04 -0300

    copy to clipboard with format

--- src/helpers/text.ts
+++ src/helpers/text.ts
@@ -1,6 +1,48 @@
 /**
+ * https://github.com/sudodoki/copy-to-clipboard/blob/02033decb3affef184672221904d2961f4efc5d9/LICENSE
  * Just copies text passed to clipboard
  */
-export const copyToClipboard = (text: string): void => {
-  navigator.clipboard.writeText(text)
+export const copyToClipboard = (
+  text: string,
+  format: 'text/html' | 'text/plain' = 'text/plain',
+  onError?: () => void,
+  onSuccess?: () => void
+): void => {
+  const range = document.createRange()
+  const selection = document.getSelection()
+
+  const mark = document.createElement('span')
+  mark.textContent = text
+  // reset user styles for span element
+  mark.style.all = 'unset'
+  // prevents scrolling to the end of the page
+  mark.style.position = 'fixed'
+  mark.style.top = '0'
+  // used to preserve spaces and line breaks
+  mark.style.whiteSpace = 'pre'
+  // do not inherit user-select (it may be `none`)
+  mark.style.userSelect = 'text'
+
+  mark.addEventListener('copy', (e) => {
+    e.stopPropagation()
+    if (format === 'text/html') {
+      e.preventDefault()
+      e.clipboardData?.clearData()
+      e.clipboardData?.setData(format, text)
+    }
+  })
+  document.body.appendChild(mark)
+
+  range.selectNodeContents(mark)
+  selection?.addRange(range)
+
+  document.execCommand('copy')
+  const successful = document.execCommand('copy')
+  if (successful) {
+    onSuccess?.()
+  } else {
+    onError?.()
+  }
+
+  document.body.removeChild(mark)
 }
--- src/stories/inputs/CopyableButton.stories.tsx
+++ src/stories/inputs/CopyableButton.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { ComponentMeta, ComponentStory, Story } from '@storybook/react'
 
+import { Email as EmailIcon } from '@mui/icons-material'
 import { Card, CardContent } from '@mui/material'
 
 import CopyableButton, { Props } from '../../components/inputs/CopyableButton'
@@ -24,4 +25,17 @@ const Template: ComponentStory<typeof CopyableButton> = (args) => {
 export const Example: Story<Props> = Template.bind({})
 Example.args = {
   text: 'Some text to copy'
+}
+
+export const WithCustomIcon: Story<Props> = Template.bind({})
+WithCustomIcon.args = {
+  text: 'Some text to copy',
+  icon: <EmailIcon />
+}
+
+export const WithCustomFormat: Story<Props> = Template.bind({})
+WithCustomFormat.args = {
+  text: '<b>Some text to copy</b>',
+  icon: <EmailIcon />,
+  format: 'text/html'
 }
