1d3b648f3ed1bfc8bc16b1040d8e7b1b18acb629
Erick Santos <erickosantos.dev@gmail.com>
2022-07-13 12:57:57 -0300

    [YURI-3137] Componente de Welcome (#44)
    Adding Welcome Component

---
+++ src/components/welcome/Welcome.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { Typography } from '@mui/material'
+
+export type Props = {
+  name: string
+}
+
+const Welcome: React.FC<Props> = ({ name }) => {
+  const greetings = [
+    'Olá',
+    'Oi',
+    'Oie',
+    'Hello',
+    'Hola',
+    'Bonjour',
+    'Buongiorno',
+    'Buenos dias',
+    'Fala',
+    'Vamos lá',
+    "Let's go",
+    'Vámonos',
+    'Have a nice day'
+  ]
+  const emojis = ['🦾🦾🦾', '😘', '🌈', '😜', '🤪', '🦾', '🙂']
+
+  return (
+    <Typography variant='h3'>
+      {`${greetings[Math.floor(Math.random() * greetings.length)]} ${
+        emojis[Math.floor(Math.random() * emojis.length)]
+      }, ${name}!`}
+    </Typography>
+  )
+}
+
+export default Welcome
