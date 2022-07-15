a76d671b5d2826c8c6f3b220c2b12ad2289517ba
KayoPereira <68305073+KayoPereira@users.noreply.github.com>
2022-07-15 13:14:05 -0300

    Fix type props of `MeetingCard` (#47)
    * adicionado props sx e modficado a tipagem dos props startAt e endAt
    
    * modificado o valor do endAt no story

--- src/stories/meetings/MeetingCard.stories.tsx
+++ src/stories/meetings/MeetingCard.stories.tsx
@@ -58,3 +58,10 @@ IntervieweeView.args = {
   ...baseProps,
   showInterviewee: false
 }
+
+export const WithDate: Story<Props> = Template.bind({})
+WithDate.args = {
+  ...baseProps,
+  startAt: new Date(2022, 12, 17, 12, 0, 0),
+  endAt: new Date(2022, 12, 17, 12, 30, 0)
+}
