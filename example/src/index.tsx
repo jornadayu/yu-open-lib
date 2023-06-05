008b6193433f4bfc59cebb564462740e97d7913d
Erick Santos <erickosantos.dev@gmail.com>
2023-06-05 15:16:29 -0300

    feat: 🎸 fix example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,15 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-import { BrowserRouter } from 'react-router-dom'
-
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
-root.render(
-  (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  )
-)
+root.render(<App />)
